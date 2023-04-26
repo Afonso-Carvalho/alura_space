3e7b5ce4485a1cf6ea37f7c8f438a7dc69c3197d
Afonso Carvalhos <afonsox57@gmail.com>
2023-04-26 16:34:30 -0300

    task_python

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+class ListandoFotografia(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda")
+    list_display_links = ("id","nome")
+    search_fields = ("nome",) # é um tupla por isso o uso obrigatorio da ","
+
+admin.site.register(Fotografia,ListandoFotografia)
