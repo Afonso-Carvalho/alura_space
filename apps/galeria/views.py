c2ccb230e665c56f18d4ea087320b2ee1db44df1
Afonso-Carvalho <afonsox57@gmail.com>
2023-05-06 03:15:50 -0300

    Refatorando codigos adicionando CRUDS e conectando a AWS

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -1,29 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from apps.galeria.models import Fotografia
 from django.contrib import messages
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-def index(request):
-    if not request.user.is_authenticated: # se a pessoa nã estiver autenticada volta ao login
-        messages.error(request, 'Usuario não logado')
-        return redirect('login')
-
-    fotografia = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
-    return render(request, 'galeria/index.html',{"cards": fotografia})
-
-def imagem(request, foto_id):
-    fotografia = get_object_or_404(Fotografia, pk=foto_id)
-    return render(request, 'galeria/imagem.html',{"fotografia": fotografia})
-
-def buscar(request):
-    if not request.user.is_authenticated: # se a pessoa nã estiver autenticada volta ao login
-        messages.error(request, 'Usuario não logado')
-        return redirect('login')
-
-=======
-=======
->>>>>>> desenvolvendo
 from apps.galeria.forms import FotografiaForms
 
 def autenticacao_requerida(view_func):
@@ -46,10 +23,6 @@ def imagem(request, foto_id):
 
 @autenticacao_requerida
 def buscar(request):
-<<<<<<< HEAD
->>>>>>> desenvolvendo
-=======
->>>>>>> desenvolvendo
     fotografia = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
 
     if 'buscar' in request.GET:
@@ -57,12 +30,6 @@ def buscar(request):
         if nome_a_buscar:
             fotografia = fotografia.filter(nome__icontains=nome_a_buscar)
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    return render(request, 'galeria/buscar.html', {"cards": fotografia})
-=======
-=======
->>>>>>> desenvolvendo
     return render(request, 'galeria/index.html', {"cards": fotografia})
 
 @autenticacao_requerida
@@ -105,8 +72,3 @@ def filtro(request, categoria):
 
     fotografia = Fotografia.objects.order_by("data_fotografia").filter(publicada=True, categoria=categoria )
     return render(request, "galeria/index.html", {"cards": fotografia})
-
-<<<<<<< HEAD
->>>>>>> desenvolvendo
-=======
->>>>>>> desenvolvendo
